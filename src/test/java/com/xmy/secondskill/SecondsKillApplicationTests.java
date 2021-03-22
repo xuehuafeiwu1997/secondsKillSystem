580bf82cfebf8d1dbf5e0b44a333fc424ab61c24
xumingyang <xumingyang@greatyun.com>
2021-03-22 13:34:51 +0800

    初始项目搭建

---
+++ src/test/java/com/xmy/secondskill/SecondsKillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xmy.secondskill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondsKillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
